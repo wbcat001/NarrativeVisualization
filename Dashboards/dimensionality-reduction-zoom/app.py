a504cbb5f4151b3e5284ace44a7ace3a38e9fc7a
wbcat <konokaze0@gmail.com>
2025-01-20 22:58:02 +0900

    app更新

--- Dashboards/dimensionality-reduction-zoom/app.py
+++ Dashboards/dimensionality-reduction-zoom/app.py
@@ -10,9 +10,10 @@ raw_data = np.random.rand(100, 5)
 # クラスの初期化
 data_manager = DataManager("data/books")
 reducer = DimensionalityReducer()
-aligner = AlignmentHandler(method="Procrustes")
+aligner = AlignmentHandler(method="No")
 animator = AnimationManager(data_manager, aligner, reducer)
 transition_data = TransitionData(data_manager.data, reducer)
+
 # annotation_manager = AnnotationManager(data_manager, aligner, reducer)
 colors = data_manager.get_colors()
 def generate_custom_colorscale(n):
